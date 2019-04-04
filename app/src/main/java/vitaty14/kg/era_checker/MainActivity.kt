63c02322fd97d19332f889579a5f00a5e826ac4b
KG <kg.vitaty14@gmail.com>
2019-04-05 00:25:17 +0900

    Update code

--- app/src/main/java/vitaty14/kg/era_checker/MainActivity.kt
+++ app/src/main/java/vitaty14/kg/era_checker/MainActivity.kt
@@ -18,20 +18,21 @@ class MainActivity : AppCompatActivity() {
             val calendar = Calendar.getInstance()
 
             val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH) + 1
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
             val hour = calendar.get(Calendar.HOUR_OF_DAY)
             val minute = calendar.get(Calendar.MINUTE)
             val second = calendar.get(Calendar.SECOND)
-            val a = calendar.get(Calendar.DAY_OF_YEAR)
 
             val year_h = calendar.get(Calendar.YEAR) - 1989
             val hour_h = calendar.get(Calendar.HOUR_OF_DAY)
 
             handler.post{
                 val time_now = findViewById<TextView>(R.id.textview_now)
-                time_now.text = "現在時刻:${year}年${hour}時${minute}分${second}秒"
+                time_now.text = "現在時刻:${year}年${month}月${day}日${hour}時${minute}分${second}秒"
 
                 val time_h = findViewById<TextView>(R.id.textView_h)
-                time_h.text = "平成始まってから:${a}年"
+                time_h.text = "平成始まってから:${year_h}年"
             }
         }
     }
